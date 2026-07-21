b40056dcfeaac082f3dd91625c81733dada4e1a2
Jeremy <jeremy2105@gmail.com>
2026-07-21 13:44:51 +0200

    Mise en style de la baniere

---
+++ kasa/src/components/Baniere.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+//importation images
+import BaniereImage from '../../style/assets/logos/baniere.png'
+
+function Baniere() {
+  
+  return (
+    <>
+     <div className="baniereBoite">
+        <h1> Chez vous, partout et ailleurs</h1>
+     </div>
+    </>
+  )
+}
+
+export default Baniere
